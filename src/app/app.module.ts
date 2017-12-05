3fd9c7172ea7f5efeda0887a40cba90ae503293a
Adel Acebal <adelacebal@yahoo.com>
2017-12-04 19:31:47 -0500

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { PlatformsComponent } from './platforms/platforms.component';
+import { ContentComponent } from './content/content.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    PlatformsComponent,
+    ContentComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
